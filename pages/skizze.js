22f258187720448759ea8d122962a76040ae3883
johannkirr <114095519+johannkirr@users.noreply.github.com>
2023-03-04 19:51:06 +0100

    fsdf

--- pages/skizze.js
+++ pages/skizze.js
@@ -2,15 +2,22 @@ import Image from 'next/image'
 import HausSkizze from '../public/bilder/haus2.png'
 import HausSkizze1 from '../public/bilder/haus1.png'
 
-export default function Skizze() {
+export default function skizze() {
+
+  
   return (
     <div>
-      <div className="skizzeBild">
-        <img src={HausSkizze} alt="Card example image"></img>
-      </div>
-      <div className="skizzeBild1">
+      <div className="skizzeBildContainer">
+   
+   <div className="skizzeBild">
+   <Image  src={HausSkizze} layout="cover" alt="Card example image"></Image>
 
-        <Image src={HausSkizze1}  fill alt="Card example image"></Image>
+   </div>
+   <div className="skizzeBild">
+   <Image src={HausSkizze1}   layout="cover" alt="Card example image"></Image>
+
+   </div>
+
       </div>
       <p className="hollow">...<br></br>...<br></br>...<br></br>...</p>
     </div>
