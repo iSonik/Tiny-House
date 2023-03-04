2797f0602f552d7d18aca2b0859da1d39297337f
johannkirr <114095519+johannkirr@users.noreply.github.com>
2023-03-04 19:11:59 +0100

    get rid of br

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -465,7 +465,7 @@ module.exports = require("react/jsx-runtime");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [210,676,664,121,675], () => (__webpack_exec__(4359)));
+var __webpack_exports__ = __webpack_require__.X(0, [121,676,61], () => (__webpack_exec__(4359)));
 module.exports = __webpack_exports__;
 
 })();
