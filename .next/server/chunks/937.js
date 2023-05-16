2853150f20f43e35f6101da75cb34fa88e87f90a
johannkirr <114095519+johannkirr@users.noreply.github.com>
2023-05-16 16:44:04 +0200

    gitignore hinzugefügt

---
+++ .next/server/chunks/937.js
@@ -0,0 +1,70 @@
+"use strict";
+exports.id = 937;
+exports.ids = [937];
+exports.modules = {
+
+/***/ 937:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const jsonInside = {
+    inside: [
+        {
+            id: 5,
+            name: "Schlafbereich",
+            url: "bild5",
+            bild: "/bilder/cardPicInside/bild8.jpg",
+            title: "Schlafbereich/Wohnbreich",
+            beschreibung: "Hier wird ein Rahmen eingezogen und sp\xe4ter eine 15mm Dreischicht Platte aufgelegt. Oben wird dann geschlafen und unten kommt eine Sitzecke hin."
+        },
+        {
+            id: 6,
+            name: "Wohnbreich unten",
+            url: "bild6",
+            bild: "/bilder/cardPicInside/bild10.jpg",
+            title: "Wohnberreich unten",
+            beschreibung: "Unten habe ich mich entschieden den Wohnberreich wei\xdf zu streichen, das l\xe4sst den Raum heller und gr\xf6\xdfer erscheinen, gleichzeitig ist es ein sch\xf6ner Kontrast. Die 5x5er Balken wurden auch miteinander quer verstrebt, dass gibt dem ganzen eine enorme Stabilit\xe4t bei geringem Gewicht. Der untere Balken wurde schief montiert, damit schafft man mehr Platz zum Durchgang zur Terassent\xfcre. "
+        },
+        {
+            id: 1,
+            name: "??? :-)",
+            url: "bild1",
+            bild: "/bilder/cardPicInside/bild11.jpg",
+            title: "??? :-)",
+            beschreibung: "Das ist kein Sessel f\xfcr das Tiny Haus ;-) , sondern die h\xf6chste Stufe der Treppe."
+        },
+        {
+            id: 2,
+            name: "Treppe",
+            url: "bild2",
+            bild: "/bilder/cardPicInside/bild12.jpg",
+            title: "Treppe",
+            beschreibung: "Die Innen und Au\xdfenw\xe4nde des Treppenkastens bestehen aus 13mm dreischichtplatten um am Gewicht zu sparen. Schlie\xdflich soll das ganze so leicht und stabil wie m\xf6glich werden."
+        },
+        {
+            id: 3,
+            name: "Treppen Stufe",
+            url: "bild3",
+            bild: "/bilder/cardPicInside/bild16.jpg",
+            title: "Treppen Stufen",
+            beschreibung: "Die Stufen sind 32cm bis 63cm breit. So kann man Platzsparend eine Treppe und einen Kasten in einem bauen. Schrankt\xfcren kommen sp\xe4ter auch noch auf den Trepenkasten"
+        },
+        {
+            id: 4,
+            name: "Aufgang zur Treppe",
+            url: "bild4",
+            bild: "/bilder/cardPicInside/bild15.jpg",
+            title: "Aufgang zur Treppe",
+            beschreibung: "Die Treppenstufen sind abgerundet und geschliffen, damit man bequem daran vorbei gehen kann. Die Stufen bestehen aus 19 mm dreischicht Platten"
+        }
+    ]
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (jsonInside);
+
+
+/***/ })
+
+};
+;
